b304f68cf748d1769be8d87c9b3917424d606ca8
Forest Sharp <forestcollinssharp@gmail.com>
2017-12-09 09:58:32 -0500

    converting vertices to glm::vec3s

--- Src/Renderer/Vulkan/VulkanBuffer.h
+++ Src/Renderer/Vulkan/VulkanBuffer.h
@@ -7,7 +7,7 @@
 //TODO: Move this. Buffer should only know about its data, shouldn't care about contents
 struct Vertex
 {
-	glm::vec2 pos;
+	glm::vec3 pos;
 	glm::vec3 color;
 	glm::vec2 texCoord;
 
@@ -30,7 +30,7 @@ struct Vertex
 
 		Descriptions[0].binding = 0;
 		Descriptions[0].location = 0;
-		Descriptions[0].format = vk::Format::eR32G32Sfloat;
+		Descriptions[0].format = vk::Format::eR32G32B32Sfloat;
 		Descriptions[0].offset = offsetof(Vertex, pos);
 
 		Descriptions[1].binding = 0;
@@ -47,14 +47,20 @@ struct Vertex
 };
 
 const std::vector<Vertex> vertices = {
-    {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-    {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-    {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-    {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}}
+    {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+    {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+    {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+    {{-0.5f, 0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}},
+
+    {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+    {{0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+    {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+    {{-0.5f, 0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}}
 };
 
 const std::vector<uint16_t> indices = {
-    0, 1, 2, 2, 3, 0
+    0, 1, 2, 2, 3, 0,
+    4, 5, 6, 6, 7, 4
 };
 
 //Type of Buffer to create
